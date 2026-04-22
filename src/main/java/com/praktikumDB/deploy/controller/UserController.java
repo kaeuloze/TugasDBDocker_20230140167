8dedbd98ce9b9b9667c86e1e14c9ea920bcc11d5
kaeuloze <mariskaesapurnomo@gmail.com>
2026-04-22 10:40:50 +0700

    Menambahkan UserController dengan endpoint POST untuk membuat data User melalui API

---
+++ src/main/java/com/praktikumDB/deploy/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.praktikumDB.deploy.controller;
+
+
+import com.praktikumDB.deploy.model.User;
+import com.praktikumDB.deploy.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody User request){
+        userService.addUser(request);
+        return "user created succesfully";
+    }
+}
